c04727e1b1de528d07ebf92c5b663a37aac128d7
Tahomi <tahomidev@gmaiil.com>
2021-01-09 18:14:18 +0200

    init

---
+++ src/main/java/com/example/demo/BezPonatiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezPonatiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezPonatiaApplication.class, args);
+	}
+
+}
